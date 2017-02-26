7939d7b1e6b7a325a50b4683abb39ded2a14db10
Arthur Hazleden <arthur.hazleden@gmail.com>
2017-02-26 00:36:24 -0800

    First upload of local code.

---
+++ D1L_firmware1_SHOP1/D1L_firmware1_SHOP1.ino
@@ -0,0 +1,252 @@
+/*
+    Eberspacher D1L Controller Firmware
+
+    Arthur Hazleden
+    December 2016
+
+    D10 - standby LED
+    D11 - startup LED
+    D12 - running LED
+    D13 - shutdown LED
+
+    D3 - flame switch sensor
+    D4 - overtemperature switch sensor
+    D5 - thermostat switch
+
+    D6 - glow plug relay, on open collector NPN
+    D - extra open collector NPN
+    D8 - fuel pump mosfet
+    D9 - blower motor PWM control mosfet
+
+    A0 - battery voltage across a 22k / 10k resistor divider network
+    A1 - cabin temperature from and LM
+*/
+
+
+// pin assignments
+const unsigned char PIN_FLAME_SWITCH = 3;
+const int overTempSwitch = 4;
+const int tStatSwitch = 5;
+
+const int glowRelay = 6;
+//const int spareOC = 7;
+const int fuelPump = 8;
+const int blowerMotor = 9;
+
+
+// variables used for timing
+unsigned long previousMillis = 0;
+
+//variables used for heater control
+int pumpRate = 0;
+int heaterState = 0;
+
+int tStatState = 0;
+int lastTStatState = 0;
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 200;
+int callForHeat = 0;  //set to 1 when the thermostat indicates heating is needed
+
+unsigned long previousPumpMillis = 0;
+
+// constants defining heater startup function timing, times are when these operations start after the thermostat engauges the heater
+const long justGlow = 500; // engauge the glowplug
+const long blowerStartup = 3000;  //start the blower on low speed
+const long startupFueling = 60000;  //start with pump at low rate
+const long glowCut = 60000; //cut glow after flame detected and this time
+const long powerIncrease = 100000; //blower and pump to medium speed at this point, warm up heater housing in prep for run mode
+const long fullPower = 260000; //blower and pump to full speed at this point, switch to run mode after this
+
+// constants defining heater shutdown function timing, times are when these operations start after the thermostat stops the heater
+const long blowerHigh = 20000; //cut fuel, engauge glowplug for cleaning and start cooling down the heater
+const long glowCleaning = 40000; //disengauge glow plug, is clean by now
+const long blowerLow = 240000;  //stop blower after the cooldown phase, proceed to standby phase
+
+
+//constants defining heater function rates (all in milliseconds, a shorter time means more pumps per second)
+const int pumpPulseTime = 100;
+const int lowFuelRate = 600;
+const int mediumFuelRate = 400;
+const int highFuelRate = 250;
+
+
+void setup() {
+
+  for (int thisPin = 3; thisPin <= 5; thisPin++) {
+    pinMode(thisPin, INPUT);
+  }
+
+  for (int thisPin = 6; thisPin <= 9; thisPin++) {
+    pinMode(thisPin, OUTPUT);
+  }
+  digitalWrite(glowRelay, LOW);
+  digitalWrite(spareOC, LOW);
+  digitalWrite(fuelPump, LOW);
+  digitalWrite(blowerMotor, LOW);
+
+}
+
+void loop() {
+
+  thermostat(); //check the temperature and state, change state if necessary
+
+  pump(); //update the super slow fuel pump PWM
+  
+  heaterStateLoop();
+
+}
+
+
+void heaterStateLoop()
+{
+
+   /* Heater States:
+     0 - standby
+     1 - startup
+     2 - running
+     3 - shutdown
+  */
+
+  unsigned long currentMillis = millis();
+
+  switch (heaterState) {
+    case 0: // ********** standby **********
+      digitalWrite(glowRelay, LOW);
+      digitalWrite(spareOC, LOW);
+      digitalWrite(blowerMotor, LOW);
+
+      pumpRate = 0;
+
+      break;
+
+    case 1: // ********** startup **********
+
+      if (currentMillis - previousMillis < justGlow) {  // engauge the glowplug
+        digitalWrite(glowRelay, HIGH);
+        previousMillis = currentMillis;
+      }
+
+      if (currentMillis - previousMillis < blowerStartup) { //start the blower on low speed
+        analogWrite(blowerMotor, 50);
+        previousMillis = currentMillis;
+      }
+
+      if (currentMillis - previousMillis < startupFueling) {  //start with pump at low rate
+        pumpRate = lowFuelRate;
+        previousMillis = currentMillis;
+      }
+
+      if (currentMillis - previousMillis < glowCut) { //cut glow after flame detected and this time
+        digitalWrite(glowRelay, LOW);
+        previousMillis = currentMillis;
+      }
+
+      if (currentMillis - previousMillis < powerIncrease) { //blower and pump to medium speed at this point, warm up heater housing in prep for run mode
+        analogWrite(blowerMotor, 150);
+        pumpRate = mediumFuelRate;
+        previousMillis = currentMillis;
+      }
+
+      if (currentMillis - previousMillis < fullPower) { //blower and pump to full speed at this point, switch to run mode after this
+        analogWrite(blowerMotor, 255);
+        pumpRate = highFuelRate;
+        previousMillis = currentMillis;
+      }
+
+      if (currentMillis - previousMillis >= fullPower) {  //advance to run phase
+        heaterState = 2;
+        previousMillis = currentMillis;
+      }
+
+
+      break;
+
+    case 2: // ********** running **********
+      //in future, add multiple power levels for PI control? avoid the glow plug current draw on startup!
+
+      break;
+
+    case 3: // ********** shutdown **********
+
+      if (currentMillis - previousMillis < blowerHigh) {  //cut fuel, engauge glowplug for cleaning and start cooling down the heater
+        analogWrite(blowerMotor, 255);
+        pumpRate = 0;
+        digitalWrite(glowRelay, HIGH);
+        previousMillis = currentMillis;
+      }
+
+      if (currentMillis - previousMillis < glowCleaning) {  //disengauge glow plug, is clean by now
+        digitalWrite(glowRelay, LOW);
+        previousMillis = currentMillis;
+      }
+
+      if (currentMillis - previousMillis >= blowerLow) {  //stop blower after the cooldown phase, proceed to standby phase
+        analogWrite(blowerMotor, 0);
+        heaterState = 0;
+        previousMillis = currentMillis;
+      }
+
+      break;
+
+  }
+}
+
+void thermostat() { //updates thermostat reading, checks for overtemperature condition, changes heater state
+
+  // ********** Debounced thermostat checkup: **********
+  int reading = digitalRead(tStatSwitch);
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastTStatState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the tStatState is at, it's been there for longer
+    // than the debounce delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != tStatState) {
+      tStatState = reading;
+
+      // only toggle the call for heat if the new tstat state is HIGH
+      if (tStatState == HIGH) {
+        callForHeat = !callForHeat;
+      }
+    }
+  }
+  // save the tStatState.  Next time through the loop,
+  // it'll be the lastTStatState:
+  lastTStatState = reading;
+
+  // ********** Heater state change: **********
+  if (callForHeat == 1 && heaterState == 0) { //if tstat says low temp and the heater is idle, start it up
+    heaterState = 1;
+  }
+
+  if (callForHeat == 0 && heaterState == 2) { //if tstat says high temp and heater is running, shut it down
+    heaterState = 3;
+  }
+
+
+  // ********** Overtemperature switch checkup: **********
+  if (overTempSwitch == 1) {
+    heaterState = 3;
+  }
+
+}
+
+void pump() { //based on the setting of the pump, adjust the time between pulses, pulse time is 100ms
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousPumpMillis >= pumpPulseTime) {
+    digitalWrite(fuelPump, LOW);
+  }
+
+  if (currentMillis - previousPumpMillis >= pumpRate) {
+    previousPumpMillis = currentMillis;
+    digitalWrite(fuelPump, HIGH);
+  }
+
+}
+
---
+++ DisplayTest/DisplayTest.ino
@@ -0,0 +1,192 @@
+/*
+  Here used to test heater display
+*/
+
+
+
+#include <LiquidCrystal.h>
+
+// ----------------- Heater -----------------
+
+int sensorPin = A0;    // select the input pin for the potentiometer
+int VBattMeasure = A1;  //input measures 1/3 of the battery voltage
+
+const int glowRelay = 6;
+const int fuelPump = 8;
+const int blowerMotor = 9;
+const int displayBacklight = 10;
+
+unsigned long previousMillis = 0;
+unsigned long previousPumpMillis = 0;
+
+int pumpRate = 0;
+
+const int pumpPulseTime = 150;
+const int lowFuelRate = 600;
+const int mediumFuelRate = 400;
+const int highFuelRate = 250;
+
+// ----------------- LCD -----------------
+LiquidCrystal lcd(13, 12, A2, A3, A4, A5);
+
+byte glowLeft[8] = {
+  0, 13, 18, 5, 5, 5, 5, 2
+};
+
+byte glowRight[8] = {
+  0, 22, 9, 20, 20, 20, 20, 8
+};
+
+/*
+  byte fuelPump[8] = {
+  28, 16, 24, 23, 21, 6, 4, 4
+  };
+*/
+
+byte aniPump1[8] = {
+  4, 4, 14, 10, 10, 10, 4, 4
+};
+byte aniPump2[8] = {
+  4, 4, 14, 14, 10, 10, 4, 4
+};
+byte aniPump3[8] = {
+  4, 4, 14, 10, 14, 10, 4, 4
+};
+byte aniPump4[8] = {
+  4, 4, 14, 10, 10, 14, 4, 4
+};
+
+byte aniFan1[8] = {
+  0, 0, 6, 6, 4, 12, 12, 0
+};
+byte aniFan2[8] = {
+  0, 0, 24, 8, 4, 2, 3, 0
+};
+byte aniFan3[8] = {
+  0, 0, 0, 24, 31, 3, 0, 0
+};
+byte aniFan4[8] = {
+  0, 0, 1, 3, 4, 24, 16, 0
+};
+
+unsigned long previousLCDMillis = 0;
+const long interval = 70;
+int aniState = 3;
+
+char ADC_Str[4];
+char Vbatt_Str[6];
+
+
+void setup() {
+  pinMode(glowRelay, OUTPUT);
+  pinMode(fuelPump, OUTPUT);
+  pinMode(blowerMotor, OUTPUT);
+
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+
+  lcd.createChar(0, glowLeft);
+  lcd.createChar(1, glowRight);
+
+  //lcd.createChar(2, fuelPump);
+
+  lcd.createChar(3, aniPump1);
+  lcd.createChar(4, aniPump2);
+  lcd.createChar(5, aniPump3);
+  lcd.createChar(6, aniPump4);
+
+  //lcd.createChar(7, aniFan1);
+  //lcd.createChar(8, aniFan2);
+  //lcd.createChar(9, aniFan3);
+  //lcd.createChar(10, aniFan4);
+  lcd.clear();
+  lcd.print("Eberspachenator");
+
+}
+
+void loop() {
+  lcd.setCursor(0, 1);
+
+  int ADC0 = analogRead(sensorPin);
+  
+  memset(ADC_Str, 0, 4);
+  sprintf(ADC_Str, "%3d", (ADC0 / 4));
+
+  lcd.print(ADC_Str);
+
+  //for (int thisChar = 0; thisChar < 4; thisChar++) {
+  //  lcd.print(ADC_Str[thisChar]);
+  // }
+
+  //lcd.write(byte(2));
+  //lcd.write(byte(3));
+  //lcd.write(byte(4));
+  //lcd.write(byte(5));
+  //lcd.write(byte(6));
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousLCDMillis >= interval) {
+
+    previousLCDMillis = currentMillis;
+
+    if (aniState < 6) {
+      aniState += 1;
+    } else {
+      aniState = 3;
+    }
+
+    lcd.setCursor(7, 1);
+    lcd.write(byte(aniState));
+    //lcd.write(byte(aniState+4));
+
+  }
+  //print the battery voltage
+  float Vbatt = analogRead(VBattMeasure) * 0.0146627;
+  lcd.setCursor(9, 1);
+  dtostrf(Vbatt, 4, 1, Vbatt_Str);
+  lcd.print(Vbatt_Str);
+  lcd.print("V");
+
+  pumpRate = 4092 - (ADC0 * 2);
+
+  if (pumpRate > 3000) {
+    digitalWrite(glowRelay, HIGH);
+    digitalWrite(displayBacklight,LOW);
+    lcd.setCursor(4, 1);
+    lcd.write(byte(0));
+    lcd.write(byte(1));
+  }
+  else if (pumpRate < 2900)
+  {
+    digitalWrite(glowRelay, LOW);
+    digitalWrite(displayBacklight,HIGH);
+    lcd.setCursor(4, 1);
+    lcd.print("  ");
+  }
+
+  analogWrite(blowerMotor, (ADC0 / 4));
+  pump();
+
+
+}
+
+
+void pump() { //based on the setting of the pump, adjust the time between pulses, pulse time is 100ms
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousPumpMillis >= pumpPulseTime) {
+    digitalWrite(fuelPump, LOW);
+    lcd.setCursor(6, 1);
+    lcd.print(" ");
+  }
+
+  if (currentMillis - previousPumpMillis >= pumpRate) {
+    previousPumpMillis = currentMillis;
+    digitalWrite(fuelPump, HIGH);
+    lcd.setCursor(6, 1);
+    lcd.write(byte(3));
+  }
+
+}
